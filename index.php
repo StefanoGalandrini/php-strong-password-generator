647d6e4a68ea3a5ab212361e4552be8248f89b18
StefanoGalandrini <stefe68@gmail.com>
2023-06-08 19:05:14 +0200

    Add function to generate random password

--- index.php
+++ index.php
@@ -1,53 +1,81 @@
-<!-- Milestone 1
-Creare un form che invii in GET la lunghezza della password. Una nostra funzione
-utilizzerà questo dato per generare una password casuale (composta da lettere,
-lettere maiuscole, numeri e simboli) da restituire all’utente.
-Scriviamo tutto (logica e layout) in un unico file index.php
-
-Milestone 2
-Verificato il corretto funzionamento del nostro codice, spostiamo la logica in
-un file functions.php che includeremo poi nella pagina principale
-
-Milestone 3 (BONUS)
-Gestire ulteriori parametri per la password: quali caratteri usare fra numeri,
-lettere e simboli. Possono essere scelti singolarmente (es. solo numeri) oppure
-possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre
-insieme).
-Dare all’utente anche la possibilità di permettere o meno la ripetizione di
-caratteri uguali. -->
-
 <?php
-// Array di lettere minuscole
-$letters = range('a', 'z');
+// Array di lettere minuscole e maiuscole
+$lett_characters = array_merge(range("a", "z"), range("A", "Z"));
 
 // Array di numeri da 0 a 9
-$numbers = range(0, 9);
+$nums_characters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// Array di simboli comuni
-$symbols = array('!', '@', '#', '$', '%', '&');
+// Stringa di simboli
+$sym_characters = ["!", "@", "#", "$", "%", "^", "&", "*"];
 
 // Variabile lunghezza input
 $pw_length = null;
-if (isset($_GET['pw-length'])) {
-	$pw_length = intval($_GET['pw-length']);
+if (isset($_GET["pw-length"])) {
+	$pw_length = intval($_GET["pw-length"]);
 }
 
 // Lettura dei campi di input
-$rep_on  = isset($_GET['rep-on']) ? $_GET['rep-on'] : '';
-$rep_off = isset($_GET['rep-off']) ? $_GET['rep-off'] : '';
-$letters = isset($_GET['letters']) ? $_GET['letters'] : '';
-$numbers = isset($_GET['numbers']) ? $_GET['numbers'] : '';
-$symbols = isset($_GET['symbols']) ? $_GET['symbols'] : '';
+$rep_on  = isset($_GET["rep_on"]) ? $_GET["rep_on"] : "";
+$rep_off = isset($_GET["rep_off"]) ? $_GET["rep_off"] : "";
+$letters = isset($_GET["letters"]) ? $_GET["letters"] : "";
+$numbers = isset($_GET["numbers"]) ? $_GET["numbers"] : "";
+$symbols = isset($_GET["symbols"]) ? $_GET["symbols"] : "";
 
 // Messaggio di errore in assenza di input validi
-$message = '';
+$message = "";
 
-if ($pw_length === 0 && $rep_on === '' && $rep_off === '' && $letters === '' && $numbers === '' && $symbols === '') {
-	$message = 'Nessun parametro valido inserito';
+if ($pw_length === 0 && $rep_on === "" && $rep_off === "" && $letters === "" && $numbers === "" && $symbols === "") {
+	$message = "Nessun parametro valido inserito";
 } else {
-	$message = "-";
+	$message = "";
 }
 
+// Array dato dall"unione delle stringhe consentite
+$characters = [];
+
+if ($letters) {
+	$characters = array_merge($characters, $lett_characters);
+}
+
+if ($numbers) {
+	$characters = array_merge($characters, $nums_characters);
+}
+
+if ($symbols) {
+	$characters = array_merge($characters, $sym_characters);
+}
+
+// Funzione che genera una password casuale
+// chiamata solo se la lunghezza in input è > 0
+function generate_random_password($characters, $pw_length, $rep_on)
+{
+	// Controllo iniziale sulla lunghezza della password e sui caratteri ripetuti
+	if ($pw_length > count($characters) && !$rep_on) {
+		$message = "La lunghezza della password non può essere maggiore del numero di caratteri unici disponibili.";
+		return $message;
+	}
+
+	$password   = "";
+	$used_chars = array();
+
+	while (strlen($password) < $pw_length) {
+		$randomIndex = rand(0, count($characters) - 1);
+		if ($rep_on || !in_array($characters[$randomIndex], $used_chars)) {
+			$password .= $characters[$randomIndex];
+			$used_chars[] = $characters[$randomIndex];
+		}
+	}
+
+	return $password;
+}
+
+// Controlla la lunghezza della password e chiama la funzione per generarla
+if ($pw_length > 0 && $message === "") {
+	$password = generate_random_password($characters, $pw_length, $rep_on);
+	$message  = "La password scelta è " . $password;
+} else {
+	$message = "Per favore, specifica una lunghezza per la password!";
+}
 
 ?>
 
@@ -65,10 +93,10 @@ if ($pw_length === 0 && $rep_on === '' && $rep_off === '' && $letters === '' && 
 		rel="stylesheet"
 		integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
 		crossorigin="anonymous">
-	<script
+	<!-- <script
 		src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
 		integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-		crossorigin="anonymous" defer></script>
+		crossorigin="anonymous" defer></script> -->
 </head>
 
 <body style="background-color: #000033; min-height: 100vh">
@@ -99,40 +127,40 @@ if ($pw_length === 0 && $rep_on === '' && $rep_off === '' && $letters === '' && 
 					<div class="col-sm-6">
 						<div class="form-check">
 							<input class="form-check-input" type="radio" name="rep_on"
-								id="rep-on" <?php if ($rep_on === 'on')
-									echo 'checked'; ?>>
-							<label class="form-check-label" for="rep-on">
+								id="repeat" <?php if ($rep_on === "on")
+									echo "checked"; ?>>
+							<label class="form-check-label" for="repeat">
 								Sì
 							</label>
 						</div>
 						<div class="form-check">
 							<input class="form-check-input" type="radio" name="rep_off"
-								id="rep-off" <?php if ($rep_off === 'on')
-									echo 'checked'; ?>>
-							<label class="form-check-label" for="rep-off">
+								id="norepeat" <?php if ($rep_off === "on")
+									echo "checked"; ?>>
+							<label class="form-check-label" for="norepeat">
 								No
 							</label>
 						</div>
 						<div class="form-check mt-3">
 							<input class="form-check-input" type="checkbox" id="letters"
-								name="letters" <?php if ($letters === 'on')
-									echo 'checked'; ?>>
+								name="letters" <?php if ($letters === "on")
+									echo "checked"; ?>>
 							<label class="form-check-label" for="letters">
 								Lettere
 							</label>
 						</div>
 						<div class="form-check">
 							<input class="form-check-input" type="checkbox" id="numbers"
-								name="numbers" <?php if ($numbers === 'on')
-									echo 'checked'; ?>>
+								name="numbers" <?php if ($numbers === "on")
+									echo "checked"; ?>>
 							<label class="form-check-label" for="numbers">
 								Numeri
 							</label>
 						</div>
 						<div class="form-check">
 							<input class="form-check-input" type="checkbox" id="symbols"
-								name="symbols" <?php if ($symbols === 'on')
-									echo 'checked'; ?>>
+								name="symbols" <?php if ($symbols === "on")
+									echo "checked"; ?>>
 							<label class="form-check-label" for="symbols">
 								Simboli
 							</label>
@@ -142,7 +170,8 @@ if ($pw_length === 0 && $rep_on === '' && $rep_off === '' && $letters === '' && 
 				<div class="form-group row">
 					<div class="col-sm-6">
 						<button type="submit" class="btn btn-primary mr-2">Invia</button>
-						<button type="reset" class="btn btn-secondary">Reset</button>
+						<a href="/php-strong-password-generator/"
+							class="btn btn-secondary">Annulla</a>
 					</div>
 				</div>
 			</form>
